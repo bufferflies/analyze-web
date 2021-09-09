50be4efd984d5d81a8014b1dc3f41a2e67085261
bufferflies <1045931706@qq.com>
2021-09-09 11:00:46 +0800

    nginx
    Signed-off-by: bufferflies <1045931706@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/Hot.vue')
+  },
+  {
+    path: '*',
+    name: 'PageNotFound',
+    component: PageNotFound
   }
+
 ]
 
 const router = new VueRouter({
